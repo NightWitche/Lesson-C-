be05972afcd6742c2462782de18bef22dbecc65c
Margo <rita-1991@mail.ru>
2022-08-22 14:43:06 +0300

    Array and Lessons C# 2

---
+++ Example_009_Leson2/Program.cs
@@ -0,0 +1,53 @@
+﻿// int a1 = 23;
+// int b1 = 11;
+// int c1 = 45;
+// int a2 = 64;
+// int b2 = 37;
+// int c2 = 67;
+// int a3 = 380;
+// int b3 = 7980;
+// int c3 = 89;
+
+// int max = a1;
+// if (b1 > max) max = b1;
+// if (c1 > max) max = c1;
+
+// if (a2 > max) max = a2;
+// if (b2 > max) max = b2;
+// if (c2 > max) max = c2;
+
+// if (a3 > max) max = a3;
+// if (b2 > max) max = b3;
+// if (c3 > max) max = c3;
+
+// Console.WriteLine(max);
+
+//(Стихийный метод нахождения максимума из 9ти чисел)
+
+// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+
+// int a1 = 23;
+// int b1 = 11;
+// int c1 = 45;
+// int a2 = 64;
+// int b2 = 37;
+// int c2 = 67;
+// int a3 = 13380;
+// int b3 = 7980;
+// int c3 = 89;
+
+// int max1 = Max (a1, b1, c1);
+// int max2 = Max (a2, b2, c2);
+// int max3 = Max (a3, b3, c3);
+// int max = Max(max1, max2, max3); // (первый вариант выбора)
+
+//int max = Max(Max (a1, b1, c1), Max (a2, b2, c2), Max (a3, b3, c3));
+//(второй метод более короткий)
+
+// Console.WriteLine(max);
---
+++ Example_Lesson2Nex/Program.cs
@@ -0,0 +1,94 @@
+﻿// Массивы в С#!!! Примеры.
+
+
+// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+//}//           0    1    2   3   4   5   6     7    8
+// int[] array = { 551, 892, 35, 48, 1556, 689, 778, 889, 90 };
+//// что бы обратится к ячейке массива
+////array[1] = 14;
+////Console.WriteLine(array[4]);
+
+// int max = Max (
+//     Max(array[0],array[1],array[2]),
+//     Max(array[3],array[4],array[5]),
+//     Max(array[6],array[7],array[8]));
+
+// Console.WriteLine(max);
+
+
+//// Имеется одномерный массив array из n элементов, 
+////требуется найти элемент массива равный find. 
+//// (а то, что несоотвествует, тоже как -то обозначить)
+
+// int[] array = {1,52,14,26,14,4,56,12,16,18};
+
+// int n = array.Length; ////длинна массива
+// int find = 14;
+
+// int index = 0;
+
+// while (index < n)
+// {
+//     if(array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break; // -преврать, когда несколько одинаковых чисел, что бы находил первое.
+//     }    
+//     index++;
+// }
+
+//// А теперь тоже самое но с синтаксисом и генератором случайных чисел (обожаю его), 
+////который заполнит массив.
+
+void FillArray(int[] collection) // коллекция аргумент для заполнения массива
+{
+    int lenght = collection.Length;
+    int index = 0; // начало
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;  //index = index+1
+    }
+}
+
+void PrintArray(int[] col) // void метод ничего не возвращает return использовать нельзя
+{
+    int count = col.Length;
+    int position = 0; // вместо index,а так тот же самый смысл
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int [] array = new int [10]; //создает новый массив в данном случае из 10 элементов.
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+                    }
+        index++;
+    }
+    return position;
+
+}
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array,5);
+Console.WriteLine(pos);
+
